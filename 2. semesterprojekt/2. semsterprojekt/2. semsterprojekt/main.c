b86ad6cdc3ae25ea6fad197d01eb0de320328701
jolle1235 <94052605+jolle1235@users.noreply.github.com>
2022-04-28 12:20:43 +0200

    oprettelsen af dokument

---
+++ 2. semesterprojekt/2. semsterprojekt/2. semsterprojekt/main.c
@@ -0,0 +1,18 @@
+/*
+ * 2. semsterprojekt.c
+ *
+ * Created: 28-04-2022 12:19:15
+ * Author : jespe
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
