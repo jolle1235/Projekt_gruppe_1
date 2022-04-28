78387794c828d3b7880590c3164eb15ef0ddb7d3
Soevndahl <104566355+Soevndahl@users.noreply.github.com>
2022-04-28 13:50:06 +0200

    Har lavet en intro

---
+++ 2. semesterprojekt/2. semsterprojekt/2. semsterprojekt/Interupt.h
@@ -0,0 +1,27 @@
+/*
+ * Interupt.h
+ *
+ * Created: 28-04-2022 13:34:12
+ *  Author: Frederik
+ */ 
+
+
+#ifndef INTERUPT_H_
+#define INTERUPT_H_
+
+
+
+ISR(INT0_vect);
+
+void initIOpins();
+
+void initINT0();
+
+
+
+
+
+
+
+
+#endif /* INTERUPT_H_ */
