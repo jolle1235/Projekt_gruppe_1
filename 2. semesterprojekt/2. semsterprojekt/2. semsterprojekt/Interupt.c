71f7e6b6759860fcb7b358566d7aa709377099f2
jolle1235 <94052605+jolle1235@users.noreply.github.com>
2022-05-03 14:33:45 +0200

    virker med zerocross
    der er lavet en funktion der er hardcoded en bit kode, som indtil videre bliver vist på lysdioderne på shieldet. den sender kun bits ved interruptet med zerocross. der er for at vi kan nå at se det sat et delay ind i funktionen

--- 2. semesterprojekt/2. semsterprojekt/2. semsterprojekt/Interupt.c
+++ 2. semesterprojekt/2. semsterprojekt/2. semsterprojekt/Interupt.c
@@ -6,17 +6,21 @@
  */ 
 
 #include "Interupt.h"
+#include <avr/io.h>
+#define F_CPU 16000000
+#include <util/delay.h>
 
 
-ISR(INT0_vect)
-{
-	PORTF = 0b00000001;
-}
+int counter = 0;
+
+
 
 void initIOpins()
 {
 	DDRF = 0xFF;		// F pins er udgange
 	DDRD = 0;			// D Pins er indgange
+	DDRB = 0xFF;
+	PORTF = 0b00000000;	// sætter 0 på alle 	
 }
 
 void initINT0()
@@ -26,5 +30,41 @@ void initINT0()
 	EICRA |= 0b00000011;
 	
 	
+}
+
+void turnOnLight(){
+	
+	//cli();
+	
+	if(counter == 0){
+		PORTB = 0b00000000;
+	}
+	if (counter == 1)
+	{
+		PORTB = 0b00000001;
+	}
+	if (counter == 2)
+	{
+		PORTB = 0b00000000;
+	}
+	if (counter == 3)
+	{
+		PORTB = 0b00000001;
+	}
+	if (counter == 4)
+	{
+		PORTB = 0b00000001;
+	}
+	if (counter == 5)
+	{
+		PORTB = 0b00000000;
+		counter = 0;
+		_delay_ms(2000);
+		PORTB = 0b11111111;
+	}
+	
+	_delay_ms(2000);
+	
+	counter++;
 }
 
