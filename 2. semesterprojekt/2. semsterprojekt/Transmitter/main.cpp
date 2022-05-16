b249f9b5367a38151028cb70d8e473aaec99a70b
Soevndahl <104566355+Soevndahl@users.noreply.github.com>
2022-05-16 14:38:38 +0200

    Transmitter sender signaler
    Transmitteren sætter et ben højt et stykke tid og slukker derefter igen indtil næste signal skal sendes

--- 2. semesterprojekt/2. semsterprojekt/Transmitter/main.cpp
+++ 2. semesterprojekt/2. semsterprojekt/Transmitter/main.cpp
@@ -15,32 +15,98 @@ ISR(INT0_vect);
 void initInterupt0();
 void initIOpins();
 
-bool zeroCross = false;
-int startbit[4] = {1,1,1,0};
 
+int startbit[4] = {1,1,1,0};
+int stopbit[4] = {0,1,1,1};
+volatile int zeroCross = 0;
 
 int main(void)
 {
+
 	initIOpins();
 	initInterupt0();
 	
     /* Replace with your application code */
     while (1) 
     {
+		
+		
+		int adresse[6] = {0,1,0,1,0,1};
+		int kommando[6] = {1,0,1,0,1,0};
+		
 		for (int i = 0; i < 4; i++)
 		{
-			while (!zeroCross)
-			{};
-			PORTB = i;
-			zeroCross = false;
+			while (zeroCross==0)
+			{}
+			PORTC = startbit[i];
+			TCCR0A |= 0b00000000;
+			TCCR0B |= 0b00000010;
+			while ((TIFR0 & (1<<0)) == 0)
+			{}
+			PORTC = 0;
+			TCCR0B |= 0b00000000;
+			TIFR0 = 0b00000001;
+			zeroCross = 0;
+		}
+
+		for (int i = 0; i < 6; i++)
+		{
+			while (zeroCross==0)
+			{}
+			PORTC = adresse[i];
+			TCCR0A |= 0b00000000;
+			TCCR0B |= 0b00000010;
+			while ((TIFR0 & (1<<0)) == 0)
+			{}
+			PORTC = 0;
+			TCCR0B |= 0b00000000;
+			TIFR0 = 0b00000001;
+			zeroCross = 0;
+		}		
+		
+		for (int i = 0; i < 6; i++)
+		{
+			while (zeroCross==0)
+			{}
+			PORTC = kommando[i];
+			TCCR0A |= 0b00000000;
+			TCCR0B |= 0b00000010;
+			while ((TIFR0 & (1<<0)) == 0)
+			{}
+			PORTC = 0;
+			TCCR0B |= 0b00000000;
+			TIFR0 = 0b00000001;
+			zeroCross = 0;
+		}
+		
+		
+		for (int i = 0; i < 4; i++)
+		{
+			while (zeroCross==0)
+			{}
+			PORTC = stopbit[i];
+			TCCR0A |= 0b00000000;
+			TCCR0B |= 0b00000010;
+			while ((TIFR0 & (1<<0)) == 0)
+			{}
+			PORTC = 0;
+			TCCR0B |= 0b00000000;
+			TIFR0 = 0b00000001;
+			zeroCross = 0;
+		}
+		
+
+		while (1)
+		{
+			PORTB = 0xFF;
 		}
     }
 }
 
 ISR(INT0_vect)
 {
+	zeroCross = 1;
 	PORTB = PINB ^ 0b10000000;
-	zeroCross = true;
 }
 
 void initInterupt0()
